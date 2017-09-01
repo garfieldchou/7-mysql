0d945ebff4671906caa592020018689b81fd034a
garfield_chou <garfield_chou@asus.com>
2017-09-01 16:56:07 +0800

    get rid of a cookie

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-	setcookie( 'customerID', '1234', time() + 60 * 60 * 24);
+	//setcookie( 'customerID', '1234', time() + 60 * 60 * 24);
+
+	setcookie( 'customerID', '', time() - 60 * 60 );
 
 	echo $_COOKIE['customerID'];
 
