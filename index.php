5bdbb01390db153033a1886a5fd23f13fa7e9cef
garfield_chou <garfield_chou@asus.com>
2017-09-01 08:53:58 +0800

    fetch items in returned query result

--- index.php
+++ index.php
@@ -9,23 +9,15 @@
 
 	}
 
-	//$query = "INSERT INTO users (email, password) VALUES('tommy@gmail.com', 'isefju&')";
-
-	$query = "UPDATE users SET password = 'qerjekal13adfU' WHERE email = 'robpercival@gmail.com' LIMIT 1";
-
-	if ( mysqli_query( $link, $query ) ) {
-	    echo "New record updated successfully";
-	} else {
-	    echo "Error: " . $query . "<br>" . mysqli_error($link);
-	}
-
 	$query = "SELECT * FROM users";
 
 	if ( $result = mysqli_query( $link, $query ) ) {
 
-		$row = mysqli_fetch_array( $result );
+		while ( $row = mysqli_fetch_array( $result ) ) {
 
-		echo "Your email is ".$row['email']." and your password is ".$row['password'];
+			print_r( $row );
+
+		}
 
 	}
 
