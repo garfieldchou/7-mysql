2d7624cc08aad0f0cfe46c2b32cfa55841ce61e2
garfield_chou <garfield_chou@asus.com>
2017-09-01 12:59:34 +0800

    Check to see if email address has been taken or not

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+	$link = mysqli_connect("localhost", "root", "root", "users");
+	// server name, user name for server, password for server, database name
+
+	if ( mysqli_connect_error() ) {
+
+		die ("There was an error connecting to the database");
+
+	}
+
 	if ( array_key_exists('email', $_POST) OR array_key_exists('password', $_POST) ) {
 
 		if ( $_POST['email'] == '' ) {
@@ -10,18 +19,22 @@
 
 			echo '<p>Password is required.</p>';
 
+		} else {
+
+
+			$query = "SELECT id FROM users WHERE email = '".mysqli_real_escape_string( $link, $_POST['email'])."'";
+
+			$result = mysqli_query( $link, $query );
+
+			if ( mysqli_num_rows( $result ) > 0 ) {
+
+				echo "<p>That mail address has already been taken.</p>";
+
+			}
+
 		}
 
-	}
-
-	$link = mysqli_connect("localhost", "root", "root", "users");
-	// server name, user name for server, password for server, database name
-
-	if ( mysqli_connect_error() ) {
-
-		die ("There was an error connecting to the database");
-
-	}
+	}	
 
 ?>
 
