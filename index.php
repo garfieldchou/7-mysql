60ef0a99e55401cf7103b9faee175f5ef596a269
garfield_chou <garfield_chou@asus.com>
2017-09-05 17:16:46 +0800

    styling for background image

--- index.php
+++ index.php
@@ -136,10 +136,25 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
     <style type="text/css">
+
 	    .container {
 	    	text-align: center;
 	    	width: 400px;
+	    	margin-top: 150px;
 	    }
+
+	    html {
+	    	background: url(background.jpg) no-repeat center center fixed; 
+	    	-webkit-background-size: cover;
+	    	-moz-background-size: cover;
+	    	-o-background-size: cover;
+	    	background-size: cover;
+	    }
+
+	    body {
+	    	background: none;
+	    }
+
     </style>
   </head>
   <body>
