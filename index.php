d5594a661584b874d12295e67eb06ba6d2f65a7c
garfield_chou <garfield_chou@asus.com>
2017-09-05 16:31:47 +0800

    use bootstrap to beautify the forms

--- index.php
+++ index.php
@@ -125,32 +125,71 @@
     
 ?>
 
-<div id="error"><?php echo $error; ?></div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<form method="post">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <body>
 
-    <input type="email" name="email" placeholder="Your Email">
-    
-    <input type="password" name="password" placeholder="Password">
-    
-    <input type="checkbox" name="stayLoggedIn" value=1>
+  	<div class="container">
 
-    <input type="hidden" name="signUp" value="1">
-    
-    <input type="submit" name="submit" value="Sign Up!">
-    
-</form>
+    	<h1>Secret Diary</h1>
 
-<form method="post">
+		<div id="error"><?php echo $error; ?></div>
 
-    <input type="email" name="email" placeholder="Your Email">
-    
-    <input type="password" name="password" placeholder="Password">
-    
-    <input type="checkbox" name="stayLoggedIn" value=1>
+		<form method="post">
 
-    <input type="hidden" name="signUp" value="0">    
-    
-    <input type="submit" name="submit" value="Log In!">
-    
-</form>
+			<div class="form-group">
+		    	<input type="email" class="form-control" name="email" placeholder="Your Email">
+		    </div>
+		    <div class="form-group">
+		    	<input type="password" class="form-control" name="password" placeholder="Password">
+		    </div>
+			<div class="form-check">
+    			<label class="form-check-label">
+    				<input type="checkbox" class="form-check-input" name="stayLoggedIn" value=1> Stay logged in
+				</label>
+		    </div>
+		    <div class="form-group">
+		    	<input type="hidden" name="signUp" value="1">
+		    	<input type="submit" class="btn btn-success" name="submit" value="Sign Up!">
+		    </div>
+		    
+		</form>
+
+		<form method="post">
+
+		    <div class="form-group">
+		    	<input type="email" class="form-control" name="email" placeholder="Your Email">
+		    </div>
+		    <div class="form-group">		    
+		    	<input type="password" class="form-control" name="password" placeholder="Password">
+		    </div>
+			<div class="form-check">
+    			<label class="form-check-label">		    
+		    		<input type="checkbox" class="form-check-input" name="stayLoggedIn" value=1> Stay logged in
+		    	</label>
+		    	<input type="hidden" name="signUp" value="0">    
+		    </div>
+		    <div class="form-group">		    
+		    	<input type="submit" class="btn btn-success" name="submit" value="Log In!">
+		    </div>
+		    
+		</form>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
+
