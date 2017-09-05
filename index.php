f57b86a4d3e829daa2b715cb378bc23e4b3c82c3
garfield_chou <garfield_chou@asus.com>
2017-09-05 16:35:51 +0800

    make text aligned center and set the width of the container

--- index.php
+++ index.php
@@ -134,6 +134,13 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <style type="text/css">
+	    .container {
+	    	text-align: center;
+	    	width: 400px;
+	    }
+    </style>
   </head>
   <body>
 
