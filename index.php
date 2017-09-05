2137447659c1add60f8a82c3ae2bf867135dd4cb
garfield_chou <garfield_chou@asus.com>
2017-09-05 09:40:19 +0800

    add another form for login

--- index.php
+++ index.php
@@ -99,3 +99,15 @@
     <input type="submit" name="submit" value="Sign Up!">
     
 </form>
+
+<form method="post">
+
+    <input type="email" name="email" placeholder="Your Email">
+    
+    <input type="password" name="password" placeholder="Password">
+    
+    <input type="checkbox" name="stayLoggedIn" value=1>
+    
+    <input type="submit" name="submit" value="Log In!">
+    
+</form>
