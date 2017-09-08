d1cc0ecdd2ee34f47db13a9f0f38aa3f7e1b2b4b
shaudai <shaudai0402@hotmail.com>
2017-09-08 21:14:00 +0800

    move part of index.php to header.php

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <style type="text/css">
+
+	    .container {
+	    	text-align: center;
+	    	width: 400px;
+	    	margin-top: 150px;
+	    }
+
+	    html {
+	    	background: url(background.jpg) no-repeat center center fixed; 
+	    	-webkit-background-size: cover;
+	    	-moz-background-size: cover;
+	    	-o-background-size: cover;
+	    	background-size: cover;
+	    }
+
+	    body {
+	    	background: none;
+	    	color: #FFF;
+	    }
+
+	    #logInForm {
+	    	display: none;
+	    }
+
+	    .toggleForms {
+	    	font-weight: bold;
+	    }
+
+    </style>
+  </head>
+  <body>
